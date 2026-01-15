82616f684fed96d01e322f2317c4d8f9e289df8e
yunzoetang <171100337+yunzoetang@users.noreply.github.com>
2026-01-15 15:27:30 +0000

    update before vercel deployment

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,10 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "1337",
-        pathname: "/uploads/**/*",
+        protocol: "https",
+        hostname: "enduring-birds-0783425995.media.strapiapp.com",
       },
     ],
   },
--- client/src/utils/get-strapi-url.ts
+++ client/src/utils/get-strapi-url.ts
@@ -1,3 +1,3 @@
 export function getStrapiURL() {
-    return process.env.STRAPI_API_URL ?? "http://localhost:1337";
+    return process.env.STRAPI_URL ?? "http://localhost:1337";
 }
